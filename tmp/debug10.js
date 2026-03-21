5b8d46c590c0fcacd3252268d6ed1b8dd8f9a956
Ali Ganey <aliganey2016000@gmail.com>
2026-03-21 09:17:53 +0300

    chore: cleanup debug scripts

--- tmp/debug10.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = 'src/components/dashboard/grading-interface.tsx';
-let content = fs.readFileSync(path, 'utf8');
-
-// Show exact content around the quiz td block
-const idx = content.indexOf('<td key={quiz.id} className="px-4 py-4 text-center">');
-console.log('Block from td start:');
-process.stdout.write(content.slice(idx, idx + 500));
